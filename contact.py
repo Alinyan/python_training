bc773d482e0f0f646464a66873b2c942a9b6fe33
Alina.Zaytseva <Alina.Zaytseva@billing.ru>
2016-05-17 11:26:16 +0300

    Разработан тест для создания нового контакта в адресной книге.
    Выделены вспомогательные методы и классы

---
+++ contact.py
@@ -0,0 +1,28 @@
+
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address_company, home_phone,
+                       mobile_phone, work_phone, fax, email2, email3, homepage, byear, ayear, address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.lastname = lastname
+        self.title = title
+        self.company = company
+        self.address_company = address_company
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.byear = byear
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
+
+
+
