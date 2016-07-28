6635506dc427a8a7f7ae72eef802a806076558e8
Alina.Zaytseva <Alina.Zaytseva@billing.ru>
2016-07-28 14:00:49 +0300

    Сгенерированы отчеты allure для групп и контактов (в папке reports)

--- test/contact.py
+++ test/contact.py
@@ -1,98 +1,123 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 import random
+import pytest
 
 
 def test_add_contact1(app, db, check_ui, json_contacts):
-    if len(db.get_list_contacts()) == 0:
+    with pytest.allure.step('Given a contact list'):
+        if len(db.get_list_contacts()) == 0:
+            app.contact.create(json_contacts)
+        old_list_contacts = db.get_list_contacts()
+    with pytest.allure.step('When I add a contact %s to the list' % json_contacts):
         app.contact.create(json_contacts)
-    old_list_contacts = db.get_list_contacts()
-    app.contact.create(json_contacts)
-    new_list_contacts = db.get_list_contacts()
-    old_list_contacts.append(json_contacts)
-    assert sorted(new_list_contacts, key=Contact.id_or_max) == sorted(old_list_contacts, key=Contact.id_or_max)
-    if check_ui:
-        app.contact.check_ui_data(new_list_contacts)
+    with pytest.allure.step('Then the new contact list is equal to the old list with the added contact'):
+        new_list_contacts = db.get_list_contacts()
+        old_list_contacts.append(json_contacts)
+        assert sorted(new_list_contacts, key=Contact.id_or_max) == sorted(old_list_contacts, key=Contact.id_or_max)
+        if check_ui:
+            app.contact.check_ui_data(new_list_contacts)
 
 def test_delete_random_contact(app, db, check_ui, data_contacts):
-    if len(db.get_list_contacts()) == 0:
-        app.contact.create(data_contacts)
-    old_list_contacts = db.get_list_contacts()
-    contact = random.choice(old_list_contacts)
-    app.contact.delete_contact_by_id(contact.id)
-    new_list_contacts = db.get_list_contacts()
-    old_list_contacts.remove(contact)
-    assert old_list_contacts == new_list_contacts
-    if check_ui:
-        app.contact.check_ui_data(new_list_contacts)
+    with pytest.allure.step('Given a non-empty contact list'):
+        if len(db.get_list_contacts()) == 0:
+            app.contact.create(data_contacts)
+        old_list_contacts = db.get_list_contacts()
+    with pytest.allure.step('Given a random contact from the list'):
+        contact = random.choice(old_list_contacts)
+    with pytest.allure.step('When I delete the contact from the list'):
+        app.contact.delete_contact_by_id(contact.id)
+    with pytest.allure.step('Then the new contact list is equal to the old contact list without the deleted contact'):
+        new_list_contacts = db.get_list_contacts()
+        old_list_contacts.remove(contact)
+        assert old_list_contacts == new_list_contacts
+        if check_ui:
+            app.contact.check_ui_data(new_list_contacts)
 
 def test_edit_random_contact(app, db, check_ui, data_contacts):
-    if len(db.get_list_contacts()) == 0:
-        app.contact.create(data_contacts)
-    old_list_contacts = db.get_list_contacts()
-    random_contact = random.choice(old_list_contacts)
-    index = old_list_contacts.index(random_contact)
-    edit_contact = Contact(firstname="Marina", middlename="Ivanovna", lastname="Ivanova", nickname="PrettyGirl",
-                           title="economist", company="OAO Dream-House", address1="Russia, Lenina 10",
-                           home_phone="8889652", mobile_phone="9261234567", work_phone="3654578", fax="9871245",
-                           email="mar@mail.ru", email2="marina@oao-dream-house.com", email3="marina_cool@dh.com",
-                           homepage="www.dream-house.com", address2="Russia, Spb, Lenina 17", phone2="145", notes="Likes to swim")
-    edit_contact.id = random_contact.id
-    app.contact.edit_contact_by_id(edit_contact)
-    assert len(old_list_contacts) == app.contact.count()
-    new_list_contacts = db.get_list_contacts()
-    old_list_contacts[index] = edit_contact
-    assert sorted(new_list_contacts, key=Contact.id_or_max) == sorted(old_list_contacts, key=Contact.id_or_max)
-    if check_ui:
-        app.contact.check_ui_data(new_list_contacts)
+    with pytest.allure.step('Given a non-empty contact list'):
+        if len(db.get_list_contacts()) == 0:
+            app.contact.create(data_contacts)
+        old_list_contacts = db.get_list_contacts()
+    with pytest.allure.step('Given a random contact from the list'):
+        random_contact = random.choice(old_list_contacts)
+    with pytest.allure.step('Given a index of the edited contact'):
+        index = old_list_contacts.index(random_contact)
+        edit_contact = Contact(firstname="Marina", middlename="Ivanovna", lastname="Ivanova", nickname="PrettyGirl",
+                               title="economist", company="OAO Dream-House", address1="Russia, Lenina 10",
+                               home_phone="8889652", mobile_phone="9261234567", work_phone="3654578", fax="9871245",
+                               email="mar@mail.ru", email2="marina@oao-dream-house.com", email3="marina_cool@dh.com",
+                               homepage="www.dream-house.com", address2="Russia, Spb, Lenina 17", phone2="145", notes="Likes to swim")
+    with pytest.allure.step('When I edit the contact %s from the list to the contact %s' % (random_contact, edit_contact)):
+        edit_contact.id = random_contact.id
+        app.contact.edit_contact_by_id(edit_contact)
+    with pytest.allure.step('Then the new contact list is equal to the old contact list with edited contact'):
+        assert len(old_list_contacts) == app.contact.count()
+        new_list_contacts = db.get_list_contacts()
+        old_list_contacts[index] = edit_contact
+        assert sorted(new_list_contacts, key=Contact.id_or_max) == sorted(old_list_contacts, key=Contact.id_or_max)
+        if check_ui:
+            app.contact.check_ui_data(new_list_contacts)
 
 def test_contact_on_edit_page(app):
-    contacts = app.contact.get_list_contacts()
-    index = random.randrange(len(contacts))
-    contact_from_home_page = app.contact.get_list_contacts()[index]
-    contact_from_edit_page = app.contact.get_contact_from_edit_page(index)
-    assert contact_from_home_page.lastname == app.contact.make_one_space(contact_from_edit_page.lastname)
-    assert contact_from_home_page.firstname == app.contact.make_one_space(contact_from_edit_page.firstname)
-    assert contact_from_home_page.all_emails == app.contact.merge_emails(contact_from_edit_page)
-    assert contact_from_home_page.all_phones == app.contact.merge_phones(contact_from_edit_page)
+    with pytest.allure.step('Given a contact list'):
+        contacts = app.contact.get_list_contacts()
+    with pytest.allure.step('Given a index of random contact'):
+        index = random.randrange(len(contacts))
+    with pytest.allure.step('Verify equal contacts from home page and contacts from edit page'):
+        contact_from_home_page = app.contact.get_list_contacts()[index]
+        contact_from_edit_page = app.contact.get_contact_from_edit_page(index)
+        assert contact_from_home_page.lastname == app.contact.make_one_space(contact_from_edit_page.lastname)
+        assert contact_from_home_page.firstname == app.contact.make_one_space(contact_from_edit_page.firstname)
+        assert contact_from_home_page.all_emails == app.contact.merge_emails(contact_from_edit_page)
+        assert contact_from_home_page.all_phones == app.contact.merge_phones(contact_from_edit_page)
 
 def test_add_contact_in_group(app, db, json_contacts, json_groups):
-    if len(db.get_list_groups()) == 0:
-        app.group.create(json_groups)
-    group = random.choice(db.get_list_groups())
-    old_list_contacts_with_group = db.get_contacts_in_group_by_name(group)
-    app.contact.create_with_add_group(json_contacts, group.name)
-    new_list_contacts_with_group = db.get_contacts_in_group_by_name(group)
-    old_list_contacts_with_group.append(json_contacts)
-    assert sorted(new_list_contacts_with_group, key=Contact.id_or_max) == sorted(old_list_contacts_with_group, key=Contact.id_or_max)
+    with pytest.allure.step('Given a non-empty contact list random group'):
+        if len(db.get_list_groups()) == 0:
+            app.group.create(json_groups)
+        group = random.choice(db.get_list_groups())
+        old_list_contacts_with_group = db.get_contacts_in_group_by_name(group)
+    with pytest.allure.step('When I add group %s for contact %s' % (group, json_contacts)):
+        app.contact.create_with_add_group(json_contacts, group.name)
+    with pytest.allure.step('Then the new groups of contact are equal to the old groups of contact list with added group for contact'):
+        new_list_contacts_with_group = db.get_contacts_in_group_by_name(group)
+        old_list_contacts_with_group.append(json_contacts)
+        assert sorted(new_list_contacts_with_group, key=Contact.id_or_max) == sorted(old_list_contacts_with_group, key=Contact.id_or_max)
 
 def test_delete_contact_from_group(app, db, json_contacts, json_groups):
-    if len(db.get_list_groups()) == 0:
-        app.group.create(json_groups)
-    group = random.choice(db.get_list_groups())
-    if len(db.get_contacts_in_group_by_name(group)) == 0:
-        app.contact.create_with_add_group(json_contacts, group.name)
-    contact = random.choice(db.get_contacts_in_group_by_name(group))
-    old_list_contacts_with_group = db.get_contacts_in_group_by_name(group)
-    old_list_contacts_without_group = db.get_contacts_not_in_group_by_name(group)
-    app.contact.delete_contact_from_group(contact.id, group.name)
-    new_list_contacts_with_group = db.get_contacts_in_group_by_name(group)
-    new_list_contacts_without_group = db.get_contacts_not_in_group_by_name(group)
-    old_list_contacts_with_group.remove(contact)
-    old_list_contacts_without_group.append(contact)
-    assert sorted(new_list_contacts_with_group, key=Contact.id_or_max) == sorted(old_list_contacts_with_group, key=Contact.id_or_max)
-    assert sorted(new_list_contacts_without_group, key=Contact.id_or_max) == sorted(old_list_contacts_without_group, key=Contact.id_or_max)
+    with pytest.allure.step('Given a non-empty contact list with random group'):
+        if len(db.get_list_groups()) == 0:
+            app.group.create(json_groups)
+        group = random.choice(db.get_list_groups())
+        if len(db.get_contacts_in_group_by_name(group)) == 0:
+            app.contact.create_with_add_group(json_contacts, group.name)
+        contact = random.choice(db.get_contacts_in_group_by_name(group))
+        old_list_contacts_with_group = db.get_contacts_in_group_by_name(group)
+        old_list_contacts_without_group = db.get_contacts_not_in_group_by_name(group)
+    with pytest.allure.step('When I delete group %s from contact %s' % (group, contact)):
+        app.contact.delete_contact_from_group(contact.id, group.name)
+    with pytest.allure.step('Then the new groups of contact are equal to the old groups of contact list with deleted group for contact'):
+        new_list_contacts_with_group = db.get_contacts_in_group_by_name(group)
+        new_list_contacts_without_group = db.get_contacts_not_in_group_by_name(group)
+        old_list_contacts_with_group.remove(contact)
+        old_list_contacts_without_group.append(contact)
+        assert sorted(new_list_contacts_with_group, key=Contact.id_or_max) == sorted(old_list_contacts_with_group, key=Contact.id_or_max)
+        assert sorted(new_list_contacts_without_group, key=Contact.id_or_max) == sorted(old_list_contacts_without_group, key=Contact.id_or_max)
 
 def test_add_group_for_contact(app, db, json_contacts, json_groups):
-    if len(db.get_list_groups()) == 0:
-        app.group.create(json_groups)
-    group = random.choice(db.get_list_groups())
-    if len(db.get_contacts_not_in_group_by_name(group)) == 0:
-        app.contact.create(json_contacts)
-    contact = random.choice(db.get_contacts_not_in_group_by_name(group))
-    old_list_contacts_with_group = db.get_contacts_in_group_by_name(group)
-    app.contact.add_group(contact.id, group.name)
-    new_list_contacts_with_group = db.get_contacts_in_group_by_name(group)
-    old_list_contacts_with_group.append(contact)
-    assert sorted(new_list_contacts_with_group, key=Contact.id_or_max) == sorted(old_list_contacts_with_group, key=Contact.id_or_max)
+    with pytest.allure.step('Given a non-empty contact list with random group'):
+        if len(db.get_list_groups()) == 0:
+            app.group.create(json_groups)
+        group = random.choice(db.get_list_groups())
+        if len(db.get_contacts_not_in_group_by_name(group)) == 0:
+            app.contact.create(json_contacts)
+        contact = random.choice(db.get_contacts_not_in_group_by_name(group))
+        old_list_contacts_with_group = db.get_contacts_in_group_by_name(group)
+    with pytest.allure.step('When I add group %s for contact %s' % (group, contact)):
+        app.contact.add_group(contact.id, group.name)
+    with pytest.allure.step('Then the new groups of contact are equal to the old groups of contact list with added group for contact'):
+        new_list_contacts_with_group = db.get_contacts_in_group_by_name(group)
+        old_list_contacts_with_group.append(contact)
+        assert sorted(new_list_contacts_with_group, key=Contact.id_or_max) == sorted(old_list_contacts_with_group, key=Contact.id_or_max)
 
--- test/group.py
+++ test/group.py
@@ -2,75 +2,95 @@
 from model.group import Group
 import random
 import re
+import pytest
 
 
 def test_add_group(app, db, check_ui, json_groups):
-    old_list_groups = db.get_list_groups()
-    app.group.create(json_groups)
-    new_list_groups = db.get_list_groups()
-    old_list_groups.append(json_groups)
-    assert sorted(new_list_groups, key=Group.id_or_max) == sorted(old_list_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(new_list_groups, key=Group.id_or_max) == sorted(app.group.get_list_groups(), key=Group.id_or_max)
+    with pytest.allure.step('Given a group list'):
+        old_list_groups = db.get_list_groups()
+    with pytest.allure.step('When I add a group %s to the list' % json_groups):
+        app.group.create(json_groups)
+    with pytest.allure.step('Then the new group list is equal to the old list with the added group'):
+        new_list_groups = db.get_list_groups()
+        old_list_groups.append(json_groups)
+        assert sorted(new_list_groups, key=Group.id_or_max) == sorted(old_list_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(new_list_groups, key=Group.id_or_max) == sorted(app.group.get_list_groups(), key=Group.id_or_max)
 
 def test_delete_group_by_id(app, db, check_ui, data_groups):
-    if len(db.get_list_groups()) == 0:
-        app.group.create(data_groups)
-    old_list_groups = db.get_list_groups()
-    group = random.choice(old_list_groups)
-    app.group.delete_group_by_id(group.id)
-    new_list_groups = db.get_list_groups()
-    old_list_groups.remove(group)
-    assert old_list_groups == new_list_groups
-    if check_ui:
-        assert sorted(new_list_groups, key=Group.id_or_max) == sorted(app.group.get_list_groups(), key=Group.id_or_max)
+    with pytest.allure.step('Given a non-empty group list'):
+        if len(db.get_list_groups()) == 0:
+            app.group.create(data_groups)
+        old_list_groups = db.get_list_groups()
+    with pytest.allure.step('Given a random group from the list'):
+        group = random.choice(old_list_groups)
+    with pytest.allure.step('When I delete the group from the list'):
+        app.group.delete_group_by_id(group.id)
+    with pytest.allure.step('Then the new group list is equal to the old group list without the deleted group'):
+        new_list_groups = db.get_list_groups()
+        old_list_groups.remove(group)
+        assert old_list_groups == new_list_groups
+        if check_ui:
+            assert sorted(new_list_groups, key=Group.id_or_max) == sorted(app.group.get_list_groups(), key=Group.id_or_max)
 
 def test_edit_first_group(app, db, check_ui, data_groups):
-    if len(db.get_list_groups()) == 0:
-        app.group.create(data_groups)
-    old_list_groups = db.get_list_groups()
+    with pytest.allure.step('Given a non-empty group list'):
+        if len(db.get_list_groups()) == 0:
+            app.group.create(data_groups)
+        old_list_groups = db.get_list_groups()
     edit_group = Group(name="Name_of_group_new", header="Header_of_group_new", footer="Footer_of_group_new")
-    edit_group.id = old_list_groups[0].id
-    app.group.edit_group_by_id(edit_group)
-    new_list_groups = db.get_list_groups()
-    old_list_groups[0] = edit_group
-    assert sorted(new_list_groups, key=Group.id_or_max) == sorted(old_list_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(new_list_groups, key=Group.id_or_max) == sorted(app.group.get_list_groups(), key=Group.id_or_max)
+    with pytest.allure.step('When I edit a first group from the list to edit group %s' % edit_group):
+        edit_group.id = old_list_groups[0].id
+        app.group.edit_group_by_id(edit_group)
+    with pytest.allure.step('Then the new group list is equal to the old group list with edited group'):
+        new_list_groups = db.get_list_groups()
+        old_list_groups[0] = edit_group
+        assert sorted(new_list_groups, key=Group.id_or_max) == sorted(old_list_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(new_list_groups, key=Group.id_or_max) == sorted(app.group.get_list_groups(), key=Group.id_or_max)
 
 def test_modify_group_name(app, db, check_ui, data_groups):
-    if len(db.get_list_groups()) == 0:
-        app.group.create(data_groups)
-    old_list_groups = db.get_list_groups()
-    random_group = random.choice(old_list_groups)
-    index = old_list_groups.index(random_group)
-    edit_group = Group(name="New group 1324")
-    edit_group.id = random_group.id
-    app.group.edit_group_by_id(edit_group)
-    new_list_groups = db.get_list_groups()
-    old_list_groups[index] = edit_group
-    assert sorted(new_list_groups, key=Group.id_or_max) == sorted(old_list_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(new_list_groups, key=Group.id_or_max) == sorted(app.group.get_list_groups(), key=Group.id_or_max)
+    with pytest.allure.step('Given a non-empty group list'):
+        if len(db.get_list_groups()) == 0:
+            app.group.create(data_groups)
+        old_list_groups = db.get_list_groups()
+    with pytest.allure.step('Given a random group from the list'):
+        random_group = random.choice(old_list_groups)
+        index = old_list_groups.index(random_group)
+        edit_group = Group(name="New group 1324")
+    with pytest.allure.step('When I edit a group %s from the list to edit group %s' % (random_group, edit_group)):
+        edit_group.id = random_group.id
+        app.group.edit_group_by_id(edit_group)
+    with pytest.allure.step('Then the new group list is equal to the old group list with edited group'):
+        new_list_groups = db.get_list_groups()
+        old_list_groups[index] = edit_group
+        assert sorted(new_list_groups, key=Group.id_or_max) == sorted(old_list_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(new_list_groups, key=Group.id_or_max) == sorted(app.group.get_list_groups(), key=Group.id_or_max)
 
 def test_modify_group_header(app, db, check_ui, data_groups):
-    if len(db.get_list_groups()) == 0:
-        app.group.create(data_groups)
-    old_list_groups = db.get_list_groups()
-    random_group = random.choice(old_list_groups)
-    index = old_list_groups.index(random_group)
-    edit_group = Group(header="New header 125484")
-    edit_group.id = random_group.id
-    app.group.edit_group_by_id(edit_group)
-    new_list_groups = db.get_list_groups()
-    old_list_groups[index] = edit_group
-    assert sorted(new_list_groups, key=Group.id_or_max) == sorted(old_list_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(new_list_groups, key=Group.id_or_max) == sorted(app.group.get_list_groups(), key=Group.id_or_max)
+    with pytest.allure.step('Given a non-empty group list'):
+        if len(db.get_list_groups()) == 0:
+            app.group.create(data_groups)
+        old_list_groups = db.get_list_groups()
+    with pytest.allure.step('Given a random group from the list'):
+        random_group = random.choice(old_list_groups)
+        index = old_list_groups.index(random_group)
+        edit_group = Group(header="New header 125484")
+    with pytest.allure.step('When I edit a group %s from the list to edit group %s' % (random_group, edit_group)):
+        edit_group.id = random_group.id
+        app.group.edit_group_by_id(edit_group)
+    with pytest.allure.step('Then the new group list is equal to the old group list with edited group'):
+        new_list_groups = db.get_list_groups()
+        old_list_groups[index] = edit_group
+        assert sorted(new_list_groups, key=Group.id_or_max) == sorted(old_list_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(new_list_groups, key=Group.id_or_max) == sorted(app.group.get_list_groups(), key=Group.id_or_max)
 
 def test_group_list(app, db):
     def clean(group):
         return Group(id=group.id, name=re.sub(r'\s+', ' ', group.name.strip()))
-    sorted_db_list = sorted(map(clean, db.get_list_groups()), key=Group.id_or_max)
-    sorted_ui_list = sorted(app.group.get_list_groups(), key=Group.id_or_max)
-    assert sorted_ui_list == sorted_db_list
+    with pytest.allure.step('Verify sorted lists group from home page and database'):
+        sorted_db_list = sorted(map(clean, db.get_list_groups()), key=Group.id_or_max)
+        sorted_ui_list = sorted(app.group.get_list_groups(), key=Group.id_or_max)
+        assert sorted_ui_list == sorted_db_list
